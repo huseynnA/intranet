1b4433b59ed49e1de07ef943fb655f5adab3390a
ramins588 <46882939+ramins588@users.noreply.github.com>
2023-10-03 13:21:06 +0400

    test1

--- resources/views/layouts/sections/menu/verticalMenu.blade.php
+++ resources/views/layouts/sections/menu/verticalMenu.blade.php
@@ -9,7 +9,7 @@ $configData = Helper::appClasses();
   <div class="app-brand demo">
     <a href="{{url('/')}}" class="app-brand-link">
       <span class="app-brand-logo demo">
-        @include('_partials.macros',["width"=>25,"withbg"=>'#666cff'])
+        <img src="{{asset('assets/img/logo.png')}}" height="70">
       </span>
       <span class="app-brand-text demo menu-text fw-bold ms-2">{{config('variables.templateName')}}</span>
     </a>
